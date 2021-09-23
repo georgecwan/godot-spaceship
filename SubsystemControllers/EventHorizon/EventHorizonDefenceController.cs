f448d78843b4aa1200c30eded95d3d14bd71c461
Jason Yuan <jason.yuan869@gmail.com>
2021-09-23 12:25:17 -0400

    change method variables to class variables

--- SubsystemControllers/EventHorizon/EventHorizonDefenceController.cs
+++ SubsystemControllers/EventHorizon/EventHorizonDefenceController.cs
@@ -6,24 +6,30 @@ public class EventHorizonDefenceController : AbstractDefenceController
     EventHorizonSensorsController SensorsController {get{return parentShip.SensorsController as EventHorizonSensorsController;}}
     EventHorizonNavigationController NavigationController {get{return parentShip.NavigationController as EventHorizonNavigationController;}}
     EventHorizonPropulsionController PropulsionController {get{return parentShip.PropulsionController as EventHorizonPropulsionController;}}    
+    float[] tubeCooldowns;
+    Vector2 shipCoordinates;
+    Vector2 shipVelocity;
+    float speed;
+    float explosionRadius;
+    
     public override void DefenceUpdate(ShipStatusInfo shipStatusInfo, TurretControls turretControls, float deltaTime)
     {
-        Vector2 shipCoordinates = shipStatusInfo.positionWithinSystem;
-        Vector2 shipVelocity = shipStatusInfo.linearVelocity;
-        float[] tubeCooldowns = new float[] {
+        shipCoordinates = shipStatusInfo.positionWithinSystem;
+        shipVelocity = shipStatusInfo.linearVelocity;
+        tubeCooldowns = new float[] {
             turretControls.GetTubeCooldown(0),
             turretControls.GetTubeCooldown(1),
             turretControls.GetTubeCooldown(2),
             turretControls.GetTubeCooldown(3),
         };
 
-        float speed = Torpedo.LaunchSpeed;
-        float explosionRadius = Torpedo.ExplosionRadius;
+        speed = Torpedo.LaunchSpeed;
+        explosionRadius = Torpedo.ExplosionRadius;
         // List<Asteroid> = EventHorizonSensorsController.asteroidsList;
     }
 
     public override void DebugDraw(Font font)
     {
-        //Student code goes here
+
     }
 }
