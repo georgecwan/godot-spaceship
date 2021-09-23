17adfcdca7362c88246ed0023a7945359868a48b
aditirajnish <aditirajnish5@gmail.com>
2021-09-23 11:58:53 -0400

    use the UFO drive to fly diagonally

--- SubsystemControllers/Enterprise/EnterprisePropulsionController.cs
+++ SubsystemControllers/Enterprise/EnterprisePropulsionController.cs
@@ -10,6 +10,12 @@ public class EnterprisePropulsionController : AbstractPropulsionController
     public override void PropulsionUpdate(ShipStatusInfo shipStatusInfo, ThrusterControls thrusters, float deltaTime)
     {
         //Student code goes here
+
+        //Enable the UFO drive override
+        thrusters.IsUFODriveEnabled = true;
+        //fly down and to the right at a speed of 141 pixels per second
+        Vector2 velocity = new Vector2(100, 100);
+        thrusters.UFODriveVelocity = velocity
     }
 
     public override void DebugDraw(Font font)
