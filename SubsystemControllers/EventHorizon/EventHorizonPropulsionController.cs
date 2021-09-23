f88daafa18ac26900e04c8105e7ff36eea4844d9
Rohan Shetty <rshetty449@gmail.com>
2021-09-23 12:21:28 -0400

    removed print

--- SubsystemControllers/EventHorizon/EventHorizonPropulsionController.cs
+++ SubsystemControllers/EventHorizon/EventHorizonPropulsionController.cs
@@ -3,23 +3,23 @@ using System;
 
 public class EventHorizonPropulsionController : AbstractPropulsionController
 {
-    EventHorizonSensorsController SensorsController {get{return parentShip.SensorsController as EventHorizonSensorsController;}}    
-    EventHorizonNavigationController NavigationController {get{return parentShip.NavigationController as EventHorizonNavigationController;}}    
-    EventHorizonDefenceController DefenceController {get{return parentShip.DefenceController as EventHorizonDefenceController;}}
+	EventHorizonSensorsController SensorsController {get{return parentShip.SensorsController as EventHorizonSensorsController;}}    
+	EventHorizonNavigationController NavigationController {get{return parentShip.NavigationController as EventHorizonNavigationController;}}    
+	EventHorizonDefenceController DefenceController {get{return parentShip.DefenceController as EventHorizonDefenceController;}}
 
-    public override void PropulsionUpdate(ShipStatusInfo shipStatusInfo, ThrusterControls thrusters, float deltaTime)
-    {
-        //Student code goes here
+	public override void PropulsionUpdate(ShipStatusInfo shipStatusInfo, ThrusterControls thrusters, float deltaTime)
+	{
+		//Student code goes here
 
-        //Enable the UFO drive override
-        thrusters.IsUFODriveEnabled = true;
-        //fly down and to the right at a speed of 141 pixels per second
-        Vector2 velocity = new Vector2(100, 100);
-        thrusters.UFODriveVelocity = velocity
-    }
+		//Enable the UFO drive override
+		thrusters.IsUFODriveEnabled = true;
+		//fly down and to the right at a speed of 141 pixels per second
+		Vector2 velocity = new Vector2(100, 100);
+		thrusters.UFODriveVelocity = velocity;
+	}
 
-    public override void DebugDraw(Font font)
-    {
-        //Student code goes here
-    }
+	public override void DebugDraw(Font font)
+	{
+		//Student code goes here
+	}
 }
--- SubsystemControllers/EventHorizon/EventHorizonSensorsController.cs
+++ SubsystemControllers/EventHorizon/EventHorizonSensorsController.cs
@@ -4,19 +4,19 @@ using System.Collections.Generic;
 
 public class EventHorizonSensorsController : AbstractSensorsController
 {
-    EventHorizonNavigationController NavigationController {get{return parentShip.NavigationController as EventHorizonNavigationController;}}    
-    EventHorizonPropulsionController PropulsionController {get{return parentShip.PropulsionController as EventHorizonPropulsionController;}}    
-    EventHorizonDefenceController DefenceController {get{return parentShip.DefenceController as EventHorizonDefenceController;}}
+	EventHorizonNavigationController NavigationController {get{return parentShip.NavigationController as EventHorizonNavigationController;}}    
+	EventHorizonPropulsionController PropulsionController {get{return parentShip.PropulsionController as EventHorizonPropulsionController;}}    
+	EventHorizonDefenceController DefenceController {get{return parentShip.DefenceController as EventHorizonDefenceController;}}
 
-    public override void SensorsUpdate(ShipStatusInfo shipStatusInfo, IActiveSensors activeSensors, PassiveSensors passiveSensors, float deltaTime)
-    {
-        //Student code goes here   
+	public override void SensorsUpdate(ShipStatusInfo shipStatusInfo, IActiveSensors activeSensors, PassiveSensors passiveSensors, float deltaTime)
+	{
+		//Student code goes here   
 
-        print("ur mom");
-    }
+		
+	}
 
-    public override void DebugDraw(Font font)
-    {
-        //Student code goes here
-    }
+	public override void DebugDraw(Font font)
+	{
+		//Student code goes here
+	}
 }
