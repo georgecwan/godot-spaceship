757a55498507a29c7266b8e760047fbfe49c06e3
Rohan Shetty <rshetty449@gmail.com>
2021-09-23 16:11:07 -0400

    large scan

--- SubsystemControllers/EventHorizon/EventHorizonSensorsController.cs
+++ SubsystemControllers/EventHorizon/EventHorizonSensorsController.cs
@@ -13,9 +13,9 @@ public class EventHorizonSensorsController : AbstractSensorsController
 	public override void SensorsUpdate(ShipStatusInfo shipStatusInfo, IActiveSensors activeSensors, PassiveSensors passiveSensors, float deltaTime)
 	{
 		//Student code goes here   
-		asteroidList = activeSensors.PerformScan(0, 360, 100);
+		asteroidList = activeSensors.PerformScan(0, 360, 1000);
 		if (asteroidList.Count > 0) 
-			GD.Print(asteroidList[0].Amplitude);
+			GD.Print(asteroidList[0].Amplitude * activeSensors.GConstant);
 		
 	}
 
