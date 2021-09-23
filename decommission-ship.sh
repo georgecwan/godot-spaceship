46d8e8427ca2bb8c60fd2233263272454c754312
Derek Rayside <drayside@uwaterloo.ca>
2021-09-23 08:22:55 -0400

    script to decommission a ship

---
+++ decommission-ship.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "NOTE: Ships should be decommissioned in reverse alphabetical order."
+echo "ALSO: Are you really sure you want to do this?"
+exit
+
+for ship in $*
+do
+    # comment out lines that mention the ship
+    sed -i -e "s/\(.*$ship.*\)/<!-- \1 -->/" \
+        'Spaceship Collaboration Activity.csproj' 
+    sed -i -e "s/\(.*$ship.*\)/\/\/\1/" \
+        Sandbox/Scripts/ShipNames.cs
+    git add \
+        'Spaceship Collaboration Activity.csproj' \
+        Sandbox/Scripts/ShipNames.cs
+
+    # zip up files for the ship
+    zip -r decommissioned-ships.zip \
+        SubsystemControllers/$ship \
+        Sandbox/Scenes/Ships/$ship*
+
+    # stage the removals
+    git rm -r \
+        SubsystemControllers/$ship \
+        Sandbox/Scenes/Ships/$ship*
+
+done
+
