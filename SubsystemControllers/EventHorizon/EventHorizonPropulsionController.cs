296c3464d56d850e8cc189db3a3329c56543e22b
zecookiez <zycm03@gmail.com>
2021-09-30 16:24:23 -0400

    Added warp gate detection/trigger to propulsion

--- SubsystemControllers/EventHorizon/EventHorizonPropulsionController.cs
+++ SubsystemControllers/EventHorizon/EventHorizonPropulsionController.cs
@@ -10,12 +10,48 @@ public class EventHorizonPropulsionController : AbstractPropulsionController
 	public override void PropulsionUpdate(ShipStatusInfo shipStatusInfo, ThrusterControls thrusters, float deltaTime)
 	{
 
-		// Enable the UFO drive override
+		// [Old testing code for reference] Enable the UFO drive override
 		thrusters.IsUFODriveEnabled = true;
 		// Set the velocity as the displacement from the target
-		Vector2 velocity = shipStatusInfo.forwardVector;
-		thrusters.UFODriveVelocity = velocity;
-		
+		thrusters.UFODriveVelocity = shipStatusInfo.forwardVector;
+
+		// Enter warp gate when we are close enough to the gate (This assumes we are travelling to a gate :x)
+		if(isInRange(shipStatusInfo.forwardVector, shipStatusInfo.shipCollisionRadius)){
+			thrusters.TriggerWarpJump();
+		}
+		//Vector2 linearVelocity = shipStatusInfo.linearVelocity;
+		//translationUpdate(shipStatusInfo, thrusters);
+	}
+	
+	public bool isInRange(Vector2 displacement, float dist){
+		float x = (float) Math.Pow((double) displacement.x, 2);
+		float y = (float) Math.Pow((double) displacement.y, 2);
+		return x + y <= Math.Pow(dist, 2);
+	}
+
+	public void translationUpdate(ShipStatusInfo shipStatusInfo, ThrusterControls thrusters)
+	{
+		/*
+			Main thruster --> goes forward in current direction
+			Port Retro and Starboard Retro --> goes backward in current direction
+			
+			Note: This won't work properly until we are able to determine the angle of the ship :x
+		*/
+		Vector2 displacement = shipStatusInfo.forwardVector;
+		// Only correct if the ship is facing right/east 
+		if(displacement.x < 0){ 
+			thrusters.MainThrottle = 0;
+			thrusters.PortRetroThrottle = 1;
+			thrusters.StarboardRetroThrottle = 1;
+		} else if(displacement.x == 0){
+			thrusters.MainThrottle = 0;
+			thrusters.PortRetroThrottle = 0;
+			thrusters.StarboardRetroThrottle = 0;
+		} else {
+			thrusters.MainThrottle = 1;
+			thrusters.PortRetroThrottle = 0;
+			thrusters.StarboardRetroThrottle = 0;
+		}
 	}
 
 	public override void DebugDraw(Font font)
