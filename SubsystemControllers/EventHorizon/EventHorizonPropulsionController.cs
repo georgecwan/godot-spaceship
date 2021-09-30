cbd3ba3f6398628c5108aa4e55051fe07c8dc6d0
Jason Yuan <jason.yuan869@gmail.com>
2021-09-30 11:43:12 -0400

    merge navigation into defence

--- SubsystemControllers/EventHorizon/EventHorizonPropulsionController.cs
+++ SubsystemControllers/EventHorizon/EventHorizonPropulsionController.cs
@@ -10,19 +10,11 @@ public class EventHorizonPropulsionController : AbstractPropulsionController
 	public override void PropulsionUpdate(ShipStatusInfo shipStatusInfo, ThrusterControls thrusters, float deltaTime)
 	{
 
-<<<<<<< HEAD
-		//Enable the UFO drive override
-		// thrusters.IsUFODriveEnabled = true;
-		//fly down and to the right at a speed of 141 pixels per second
-		// Vector2 velocity = new Vector2(100, 100);
-		// thrusters.UFODriveVelocity = velocity;
-=======
 		// Enable the UFO drive override
 		thrusters.IsUFODriveEnabled = true;
 		// Set the velocity as the displacement from the target
 		Vector2 velocity = shipStatusInfo.forwardVector;
 		thrusters.UFODriveVelocity = velocity;
->>>>>>> origin/master
 	}
 
 	public override void DebugDraw(Font font)
