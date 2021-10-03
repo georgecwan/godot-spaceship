a809b59281c79ec721f138dd7bf48ba81a5e68e0
Brianna Wang <74729817+b-ultramarine@users.noreply.github.com>
2021-10-03 13:30:01 -0400

    fixed warp gate and added info to warp gate + planet data list

--- SubsystemControllers/EventHorizon/EventHorizonSensorsController.cs
+++ SubsystemControllers/EventHorizon/EventHorizonSensorsController.cs
@@ -1,3 +1,5 @@
+// lots of stuff is referenced from other sensors team
+
 using Godot;
 using System;
 using System.Collections.Generic;
@@ -7,10 +9,6 @@ public class EventHorizonSensorsController : AbstractSensorsController
     EventHorizonNavigationController NavigationController {get{return parentShip.NavigationController as EventHorizonNavigationController;}}
     EventHorizonPropulsionController PropulsionController {get{return parentShip.PropulsionController as EventHorizonPropulsionController;}}
     EventHorizonDefenceController DefenceController {get{return parentShip.DefenceController as EventHorizonDefenceController;}}
-
-    // public List<EMSReading> smallSpaceObjects = new List<EMSReading>();
-    // public List<EMSReading> bigSpaceObjects = new List<EMSReading>();
-    // public List<float> warpGateHeadings = new List<float>();
 
     public List<PassiveSensorReadingData> passiveSensorReadingData = new List<PassiveSensorReadingData>();
     public List<EMSReading> planetRawData = new List<EMSReading>();
@@ -23,7 +21,7 @@ public class EventHorizonSensorsController : AbstractSensorsController
         passiveSensors.GeneratePassiveSensorReadings();
         List<PassiveSensorReading> passiveSensorReadings = passiveSensors.PassiveReadings;
 
-        GD.Print("PASSIVE SENSOR READING DATA: \n");
+        // PASSIVE SENSOR READING DATA:
 
         Vector2 shipVelocity = shipStatusInfo.linearVelocity;
         float shipHeading = (float) Math.Atan2(shipVelocity.y, shipVelocity.x);
@@ -38,10 +36,7 @@ public class EventHorizonSensorsController : AbstractSensorsController
             PassiveSensorReadingData newPassiveSensorReadingData = new PassiveSensorReadingData(adjustedHeading, passiveSensorReading.ContactID);
             passiveSensorReadingData.Add(newPassiveSensorReadingData);
 
-            GD.Print("\nContact ID: " + newPassiveSensorReadingData.contactID.ToString() + ". Adjusted Heading: " + newPassiveSensorReadingData.heading.ToString() + "\n");
-
             if(passiveSensorReading.Signature.ToString() == "Planetoid") {
-                GD.Print("inside for if 1\n");
                 planetRawData = activeSensors.PerformScan(
                     adjustedHeading,
                     scanAngle,
@@ -50,84 +45,41 @@ public class EventHorizonSensorsController : AbstractSensorsController
             }
 
             if(passiveSensorReading.Signature.ToString() == "WarpGate") {
-                GD.Print("inside for if 2\n");
                 warpGateRawData = activeSensors.PerformScan(
                     adjustedHeading,
                     scanAngle,
                     scanDistance
                 );
             }
-
-            // bigSpaceObjects = activeSensors.PerformScan(
-                
-            // )
-
-            // output will be list of objects containing proper heading and contact id
-
-            // GD.Print("\nPASSIVE SENSOR READINGS: \n");
-            // GD.Print(passiveSensorReading.Signature.ToString() + "  " + passiveSensorReading.Heading.ToString());
-
         }
 
         foreach(EMSReading a in planetRawData) {
-			GD.Print("inside for loop 1\n");
 			// Calculate the position of the planet in cartesian coordinates
 			float x = (float)Math.Cos(a.Angle) * a.Amplitude * activeSensors.GConstant;
 			float y = (float)Math.Sin(a.Angle) * a.Amplitude * activeSensors.GConstant;
 			
-			PlanetData newPlanet = new PlanetData("planet", new Vector2(x, y)); // TODO: pass additional composition information needed
+			PlanetData newPlanet = new PlanetData("Planet", new Vector2(x, y), a.ScanSignature); // TODO: change name
 
             planetList.Add(newPlanet);
 
 		}
 
         foreach(EMSReading a in warpGateRawData) {
-			GD.Print("inside for loop 2\n");
-			// Calculate the position of the planet in cartesian coordinates
+			// Calculate the position of the warp gate in cartesian coordinates
 			float x = (float)Math.Cos(a.Angle) * a.Amplitude * activeSensors.GConstant;
 			float y = (float)Math.Sin(a.Angle) * a.Amplitude * activeSensors.GConstant;
-			
-            GD.Print("x " + x.ToString() + " y " + y.ToString());
 
-			WarpGateData newWarpGate = new WarpGateData("warp gate", new Vector2(x, y), "destination"); // TODO: pass additional composition information needed
+			WarpGateData newWarpGate = new WarpGateData("WarpGate to " + a.SpecialInfo, new Vector2(x, y), a.SpecialInfo);
 
             warpGateList.Add(newWarpGate);
 
 		}
 
-
-        foreach(EMSReading planetData in planetRawData) {
-            GD.Print("scan signature: " + planetData.ScanSignature);
-        }
-
-        // get contact id and heading from passiveSensorReadings, 
-        // and use the heading to perform an active scan - since they have contact id they know ht ecircumference of the object - the second parameeter to the scan is the arc length, and because the size of the beam uses energy we want to match what we're looking for
-
         // HARD CODED DATA FOR GALAXY ALPHA:
         WarpGateData solSystemWarpGate = new WarpGateData("WarpGate to Alpha Centauri System", new Vector2( (float)800,(float)0 ), "Alpha Centauri System");
         WarpGateData alphaCentauriSystemWarpGate = new WarpGateData("WarpGate to Kepler 438 System", new Vector2( (float)312.978, (float)386.366 ), "Kepler 438 System");
 
-        GD.Print("\nWARP GATE DATA: \n");
-        GD.Print("Name: " + solSystemWarpGate.name + ". Destination: " + solSystemWarpGate.destinationSolarSystemName.ToString() + ". Position: " + solSystemWarpGate.position.ToString() + ".");
-        GD.Print("Name: " + alphaCentauriSystemWarpGate.name + ". Destination: " + alphaCentauriSystemWarpGate.destinationSolarSystemName.ToString() + ". Position: " + alphaCentauriSystemWarpGate.position.ToString() + ".");
-
-        PlanetData kepler438Planet = new PlanetData("Planet Kepler 438", new Vector2( (float)1276.38, (float)107.665 ));
-
-        GD.Print("\nPLANET WARP DATA: \n");
-        GD.Print("Name: " + kepler438Planet.name + ". Position: " + kepler438Planet.position.ToString() + ".\n");
-
-        // foreach (PassiveSensorReading bigSpaceObject in bigSpaceObjects) {
-        //     string printing = "";
-        //     for (int i = 0; i < bigSpaceObjects.Count; i++) {
-        //         PassiveSensorReading psr = bigSpaceObjects[i];
-        //         printing += (psr.Signature.ToString() + " done ");
-        //         if (psr.Signature.ToString() == "WarpGate") {
-        //             warpGateHeadings.append
-        //         }
-        //     }
-        //     GD.Print(printing);
-        // }
-
+        PlanetData kepler438Planet = new PlanetData("Planet Kepler 438", new Vector2( (float)1276.38, (float)107.665 ), "Common:70|Metal:20|Water:10");
     }
 
     public override void DebugDraw(Font font)
@@ -139,22 +91,24 @@ public class EventHorizonSensorsController : AbstractSensorsController
 public struct WarpGateData {
     public String name;
 	public Vector2 position;
-	public String destinationSolarSystemName;
+	public String specialInfo;
 
-	public WarpGateData(String n, Vector2 p, String d) {
+	public WarpGateData(String n, Vector2 p, String s) {
 		name = n;
         position = p;
-		destinationSolarSystemName = d;
+		specialInfo = s;
 	}
 }
 
 public struct PlanetData {
     public String name;
 	public Vector2 position;
+    public String scanSignature;
 
-	public PlanetData(String n, Vector2 p) {
+	public PlanetData(String n, Vector2 p, String s) {
 		name = n;
 		position = p;
+        scanSignature = s;
 	}
 }
 
